0a244aa2309240b5c5e6abd6781204860503c62f
roshidhmohammed <115979576+roshidhmohammed@users.noreply.github.com>
2023-11-03 04:59:02 +0530

    final  update done

--- backend/utils/partnerToken.js
+++ backend/utils/partnerToken.js
@@ -1,21 +1,21 @@
 //  create token and save it in  cookies
 
 const sendPartnerToken = (user, statusCode, res) => {
-    const token = user.getJwtToken()
+  const token = user.getJwtToken();
 
-    // options for cookies
-    const options = {
-        expires: new Date(Date.now() + 90 * 24 * 60  * 60* 1000),
-        httpOnly: true,
-        // sameSite:"none",
-        secure:true
-    }
+  // options for cookies
+  const options = {
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    sameSite: "none",
+    secure: true,
+  };
 
-    res.status(statusCode).cookie("partner_token", token, options).json({
-        success:true,
-        user,
-        token
-    })
-}
+  res.status(statusCode).cookie("partner_token", token, options).json({
+    success: true,
+    user,
+    token,
+  });
+};
 
-module.exports = sendPartnerToken
+module.exports = sendPartnerToken;
